f40a05418c2c2f823b0ba164dd7f08df29924ff9
Giovane Santos <giovane98sp@gmail.com>
2023-10-25 12:00:28 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias <= 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`;
+}
+
+// Exemplo de uso da função:
+const vitorias = 60;
+const derrotas = 20;
+const resultado = calcularNivel(vitorias, derrotas);
+
+// Exibirá a mensagem de acordo com o nível
+console.log(resultado); 
